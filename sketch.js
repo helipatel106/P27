b83bf784ef7e58c4df36a497a5543b717ff9fb75
helipatel106 <71474857+helipatel106@users.noreply.github.com>
2020-11-19 16:44:58 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+ bobObject1=new Bob(200,250,30);
+ bobObject2=new Bob(300,300,30);
+ bobObject3=new Bob(350,325,30);
+ bobObject4=new Bob(420,400,30);
+ bobObject5=new Bob(475,450,30);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  bobObject1.display();
+  bobObject2.display();
+  bobObject3.display();
+  bobObject4.display();
+  bobObject5.display();
+  
+  drawSprites();
+ 
+}
+
+
+
